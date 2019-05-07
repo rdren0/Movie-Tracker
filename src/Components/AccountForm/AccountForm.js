5c0ab997e236e5c1957720f800de2f340603d237
Rachael Drennan <rdrennan0@gmail.com>
2019-05-07 16:23:46 -0600

    handleChange

--- src/Components/AccountForm/AccountForm.js
+++ src/Components/AccountForm/AccountForm.js
@@ -1,22 +1,40 @@
 import React, {Component} from 'react';
+import { fetchCall } from '../../APICalls/APICalls';
+
+
 
 class AccountForm extends Component {
-
   constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.type]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetchCall()
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
   }
 
   render() {
     return (
       <div>
-      <p> testing</p>
-      <input type="email" placeholder="Email"/>
-      <input type="password" placeholder="Password"/>
-      <button> Sign in </button> 
-        
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="email" placeholder="Email" value ={this.state.email}/>
+          <input onChange={this.handleChange} type="password" placeholder="Password" value={this.state.password}/>
+          <button> Sign in </button> 
+        </form>
       </div>
     );
   }
 }
-
 export default AccountForm;
