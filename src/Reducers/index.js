7341dc862402158f3532c563800d4e5372b76854
Joe Johnson <johnson.joseph527@gmail.com>
2019-05-07 23:28:36 -0600

    State will now save fetch to global state

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux'
-
+import { moviesReducer } from './moviesReducer'
 
 const rootReducer= combineReducers({
-  //stuff
+  movies: moviesReducer
 })
 
 export default rootReducer;
