563be1ab2af9fb7564ed73ca864bdb3a816be06a
Rachael Drennan <rdrennan0@gmail.com>
2019-05-14 23:44:12 -0600

    console log removal

--- src/Reducers/moviesReducer.js
+++ src/Reducers/moviesReducer.js
@@ -6,7 +6,6 @@ export const moviesReducer = (state = [], action) => {
         favorited: action.favorited
       }));
     default:
-      console.log("no movies to add");
       return state;
   }
 };
